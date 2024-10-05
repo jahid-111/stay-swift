36394f59fc6422454d2a55b7c64c5113aa105e06
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-10-05 15:19:26 +0300

    multiple_root_layout

---
+++ app/(home)/layout.js
@@ -0,0 +1,34 @@
+import localFont from "next/font/local";
+import "../globals.css";
+import Navbar from "@/components/Navbar";
+import { dbConnect } from "@/service/mongo";
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "StaySwift",
+  description: "One Place Stop for Hospitability",
+};
+
+export default async function RootLayout({ children }) {
+  dbConnect();
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar sideMenu={true} />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
