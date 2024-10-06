1cc4a5713bc02546ba531da98b44aa9cfddd3dc8
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-10-06 11:17:19 +0300

    SignOut_user

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import { auth } from "@/auth";
+import Logout from "./auth/Logout";
 
-const Navbar = ({ sideMenu }) => {
+const Navbar = async ({ sideMenu }) => {
+  const session = await auth();
+
+  console.log(session);
   return (
     <nav>
       <Link href="/">
@@ -32,9 +37,18 @@ const Navbar = ({ sideMenu }) => {
           </li>
 
           <li>
-            <Link href="/login" className="login">
-              Login
-            </Link>
+            {session?.user ? (
+              <div>
+                <span className="text-red-600">{session?.user?.name}</span>
+                <span> | </span>
+
+                <Logout />
+              </div>
+            ) : (
+              <Link href="/login" className="login">
+                Login
+              </Link>
+            )}
           </li>
         </ul>
       )}
